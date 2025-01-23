9235faa15fa7648dbab92d477537154aa16ed690
Ritik Singh <106028665+Ritiksingh19@users.noreply.github.com>
2025-01-23 15:25:19 +0530

    Add files via upload

---
+++ Day3/CircleArea.java
@@ -0,0 +1,21 @@
+class CircleArea {
+    public static void main(String[] args) {
+       
+        Scanner scanner = new Scanner(System.in);
+
+        
+        double pi = 3.14;
+
+        
+        System.out.print("Enter the radius of the circle: ");
+        double radius = scanner.nextDouble();
+
+
+        double area = pi * radius * radius;
+
+
+        System.out.println("The area of the circle is: " + area);
+        
+     
+            }
+}
---
+++ Day3/Percal.java
@@ -0,0 +1,13 @@
+ class PercentageCalculator {
+    public static void main(String[] args) {
+       
+        int totalMarks = 500;
+        int marksObtained = 430;
+
+      
+        float percentage = ( marksObtained / totalMarks) * 100;
+
+       
+        System.out.println("The percentage is: " + percentage + "%");
+    }
+}
