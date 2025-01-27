1acff42b3694857e7d469be31305c070f2f53064
Ritik Singh <106028665+Ritiksingh19@users.noreply.github.com>
2025-01-27 14:41:23 +0530

    Add files via upload

---
+++ Day5/AssignmentDemo.java
@@ -0,0 +1,29 @@
+// Assignment Operators
+
+class AssignmentDemo
+{
+
+	public static void main(String args[])
+
+{
+
+	int a = 0;
+
+        System.out.println("a :" +a);
+	a+=10;
+	
+	System.out.println("a :" +a);
+	a-=20;
+
+	System.out.println("a :" +a);
+	a*=10;
+
+	System.out.println("a :" +a);
+	a/=2;
+
+	System.out.println("a :" +a);
+	a%=4;
+
+ 	System.out.println("a :" +a);
+}
+}
